ecad41e8f8b426b9567a1e2e5e61d3506823552b
Emmanouil Konstantinidis <ekonstantinidis@outlook.com>
2014-08-15 21:53:30 +0100

    Header, menu & social settings, mobile css

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Emmanouil Konstantinidis'
 SITENAME = u'I am Emmanouil'
-SITEURL = ''
+SITEURL = 'http://www.iamemmanouil.com'
 
 PATH = 'content'
 
@@ -17,22 +17,44 @@ FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
+# Menu
+MENUITEMS = (
+    ('About', 'about'),
+    ('Home', 'start'),
+    ('Get in touch', 'get-in-touch'),
+)
+
 # Theme
 OUTPUT_PATH = "output"
 THEME = "theme"
 THEME_STATIC_DIR = "static"
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (
+    ('Pelican', 'http://getpelican.com/'),
+    ('Python.org', 'http://python.org/'),
+    ('Jinja2', 'http://jinja.pocoo.org/'),
+    ('You can modify those links in your config file', '#'),
+)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+    ('fa-twitter', 'http://twitter.com/ekondis'),
+    ('fa-github', 'http://github.com/ekonstantinidis'),
+    ('fa-linkedin', 'http://linkedin.com/uk/ekonstantinidis'),
+    ('fa-rss', '#'),
+)
 
 DEFAULT_PAGINATION = 5
+
+# Google Analytics
+GOOGLE_ANALYTICS= "UA-6891078-38"
+
+# Disqus Comments
+DISQUS_SITENAME = "iamemmanouil"
+
+# Twitter Settings
+TWITTER_USERNAME = "ekondis"
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
