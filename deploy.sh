d090f293124970c85a41e493d3248ffa74f42a53
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-02-18 20:51:49 +0000

    Fix Travis

--- deploy.sh
+++ deploy.sh
@@ -27,13 +27,9 @@ echo -e "$VARNAME"
     git clean -f -d
     git commit -m "Rel 1.5 - Empty the branch before pushing($TRAVIS_BUILD_NUMBER)"
     git push -fq origin $BRANCH > /dev/null
+    cd ..
 
     echo -e "Starting deployment on Github Pages\n"
-
-    if [ "$TRAVIS" == "true" ]; then
-        git config --global user.email "manos@iamemmanouil.com"
-        git config --global user.name "ekonstantinidis"
-    fi
 
     # Using token clone gh-pages branch
     git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null
