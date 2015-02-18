f2c0892892e8bd33f2994680d7cfa991e1da48e3
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-02-18 20:41:55 +0000

    Merge emptyrepo.sh and deploy.sh
    Also remove fable and makefile

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,28 @@ PELICAN_OUTPUT_FOLDER=output
 echo -e "Testing travis-encrypt"
 echo -e "$VARNAME"
 
-# if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+
+    echo -e "Remove previous version of website\n"
+
+    if [ "$TRAVIS" == "true" ]; then
+        git config --global user.email "manos@iamemmanouil.com"
+        git config --global user.name "ekonstantinidis"
+    fi
+
+    # Using token clone gh-pages branch
+    git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null
+
+    # Go into directory and copy data we're interested in to that directory
+    cd built_website
+    rsync -rv --exclude=.git  ../$PELICAN_OUTPUT_FOLDER/* .
+
+    # Remove everything from the "master" branch
+    git rm -rf .
+    git clean -f -d
+    git commit -m "Rel 1.5 - Empty the branch before pushing($TRAVIS_BUILD_NUMBER)"
+    git push -fq origin $BRANCH > /dev/null
+
     echo -e "Starting deployment on Github Pages\n"
 
     if [ "$TRAVIS" == "true" ]; then
@@ -27,4 +48,4 @@ echo -e "$VARNAME"
     git push -fq origin $BRANCH > /dev/null
 
     echo -e "Deploy completed\n"
-# fi
+fi
