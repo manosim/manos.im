da4e945537466a5f7fe242144c68e7e7fd1a6759
Emmanouil Konstantinidis <manos@dabapps.com>
2015-06-21 04:29:54 +0300

    Use Grunt for LESS

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function (grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.initConfig({
+
+    less: {
+      options: {
+        cleancss: true
+      },
+      main: {
+        files: {
+          '_site/styles/base.css': '_less/_base.less',
+        }
+      }
+    },
+
+    watch: {
+      files: ['_less/*.less'],
+      tasks: ['less'],
+      options: {
+        livereload: 35729,
+      }
+    },
+
+    clean: {
+      build: {
+        src: ["_site/*"]
+      }
+    }
+
+  });
+
+  grunt.registerTask('build', ['clean', 'less']);
+  grunt.registerTask('default', ['build', 'watch']);
+
+};
