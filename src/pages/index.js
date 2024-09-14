6d6bcf95598bf19b8b22e1c6bd730fbeacadafd1
Manos Konstantinidis <manosim@users.noreply.github.com>
2024-09-15 01:26:16 +0300

    feat: update homepage details

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,25 +5,34 @@ import { SEO } from '../components/Seo';
 
 const IndexPage = ({ location }) => (
   <Layout location={location}>
-    <SEO title="Engineering Manager at NearForm, London" />
+    <SEO title="Head of Engineering at Travelex" />
 
     <div className="flex flex-1 flex-col justify-center items-center bg-primary text-white">
       <div className="max-w-xl mx-12 my-5">
         <h1 className="font-display text-5xl">Hello, I'm Manos.</h1>
         <div className="mt-4 font-medium text-xl">
-          Engineering Manager at{' '}
+          Head of Engineering at{' '}
           <a
             className="text-nearform hover:text-gray-900"
-            href="https://www.nearform.com/"
+            href="https://www.travelex.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            NearForm
+            Travelex
           </a>
         </div>
 
         <div className="mt-2 font-light text-sm">
           Previously at{' '}
+          <a
+            className="hover:text-blue-400"
+            href="https://www.nearform.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Nearform
+          </a>
+          ,{' '}
           <a
             className="hover:text-blue-400"
             href="https://www.formidable.com/"
